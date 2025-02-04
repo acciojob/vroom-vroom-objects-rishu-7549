b71a5870b845d1385315cf83ad9cf36a4e3678c8
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-04 11:35:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
+Car.prototype.getMakeModel=fuction(){
+	return `${this.make}  ${this.model}`;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(make,model,this);
+	this.topSpeed = topSpeed;
+}
+SportsCar.prototype = Object.create(Car.prototype);
+
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed=fuction()(){
+	return `${this.topSpeed}`;
+}
+
 
 // Do not change the code below
 window.Car = Car;
